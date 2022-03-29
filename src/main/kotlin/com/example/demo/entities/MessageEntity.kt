2653f8b0c5836317cbaf39f7902e3292e3d9bbd1
estherwl <estherlucena@gmail.com>
2022-03-29 15:41:43 -0300

    add repository and entity

---
+++ src/main/kotlin/com/example/demo/entities/MessageEntity.kt
@@ -0,0 +1,16 @@
+package com.example.demo.entities
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class MessageEntity(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
+    val date: String,
+    val text: String
+) {
+    constructor() : this(0, "", "")
+}
